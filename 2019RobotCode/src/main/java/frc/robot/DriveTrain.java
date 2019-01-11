baaacc19868d9a3231071b5170c61e16fd21677a
Drew <dfriend44@gmail.com>
2019-01-10 19:30:56 -0500

    Added drive mode toggle to DriveTrain class

--- 2019RobotCode/src/main/java/frc/robot/DriveTrain.java
+++ 2019RobotCode/src/main/java/frc/robot/DriveTrain.java
@@ -5,23 +5,18 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 public class DriveTrain {
     public enum driveModes { tankDrive, singleArcadeDrive, dualArcadeDrive };
 	public driveModes currentDriveMode = driveModes.tankDrive;
-    private driveModes prevDriveMode = currentDriveMode;
     
 	private final int LEFT_ENCODER_CHANNEL_A = 0;
 	private final int RIGHT_ENCODER_CHANNEL_A = 1;
 	private final int LEFT_ENCODER_CHANNEL_B = 3;
 	private final int RIGHT_ENCODER_CHANNEL_B = 2;
 	public final int ENCODER_COUNTS_PER_REV = 205;
-	public final double WHEEL_DIAMETER = 6;
+    public final double WHEEL_DIAMETER = 6;
 
-	private final static int LEFT_DRIVE_PORT_1 = 0;
-	private final static int LEFT_DRIVE_PORT_2 = 3;
-	private final static int RIGHT_DRIVE_PORT_1 = 2;
-	private final static int RIGHT_DRIVE_PORT_2 = 13;
-	public Spark1038 leftDrive1 = new Spark1038(LEFT_DRIVE_PORT_1);
-	private Spark1038 leftDrive2 = new Spark1038(LEFT_DRIVE_PORT_2);
-	public Spark1038 rightDrive1 = new Spark1038(RIGHT_DRIVE_PORT_1);
-	private Spark1038 rightDrive2 = new Spark1038(RIGHT_DRIVE_PORT_2);
+	private final static int LEFT_DRIVE_PORT = 0;
+	private final static int RIGHT_DRIVE_PORT = 1;
+	public Spark1038 leftDrive1 = new Spark1038(LEFT_DRIVE_PORT);
+	public Spark1038 rightDrive1 = new Spark1038(RIGHT_DRIVE_PORT);
 	
 	private Encoder1038 leftDriveEncoder = new Encoder1038(LEFT_ENCODER_CHANNEL_A, LEFT_ENCODER_CHANNEL_B, false, ENCODER_COUNTS_PER_REV, WHEEL_DIAMETER);
 	private Encoder1038 rightDriveEncoder = new Encoder1038(RIGHT_ENCODER_CHANNEL_A, RIGHT_ENCODER_CHANNEL_B, false, ENCODER_COUNTS_PER_REV, WHEEL_DIAMETER);
@@ -60,23 +55,26 @@ public class DriveTrain {
 
     // Switch between drive modes
     public void driveModeToggler() {
-        if (currentDriveMode == driveModes.tankDrive && prevDriveMode != driveModes.tankDrive) {
-			currentDriveMode = driveModes.dualArcadeDrive;
-			prevDriveMode = currentDriveMode;
-		}	
-		else if (currentDriveMode == driveModes.dualArcadeDrive && prevDriveMode != driveModes.dualArcadeDrive) {
-			currentDriveMode = driveModes.singleArcadeDrive;
-			prevDriveMode = currentDriveMode;
-		}			
-		else if (currentDriveMode == driveModes.singleArcadeDrive && prevDriveMode != driveModes.singleArcadeDrive) {
-			currentDriveMode = driveModes.tankDrive;	
-			prevDriveMode = currentDriveMode;
-		}
+
+        switch (currentDriveMode) {
+            case tankDrive:
+                currentDriveMode = driveModes.singleArcadeDrive;
+                break;
+            case singleArcadeDrive:
+                currentDriveMode = driveModes.dualArcadeDrive;
+                break;
+            case dualArcadeDrive:
+                currentDriveMode = driveModes.tankDrive;
+                break;
+            default:
+                System.out.println("Help I have fallen and I can't get up!");
+                break;
+        }
     }
 
     // Drive robot with tank controls (input range -1 to 1 for each stick)
 	public void tankDrive(double leftStickInput, double rightStickInput) {
-        differentialDrive.tankDrive(leftStickInput, rightStickInput, true);
+        differentialDrive.tankDrive(leftStickInput * 0.57, rightStickInput * 0.57, true);
     }
     
     // Drive robot using a single stick (input range -1 to 1)
